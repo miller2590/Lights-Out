d661b68bc710b2d254ac6c4c4450c19dbd5e6187
miller2590 <miller2590@gmail.com>
2021-11-27 17:42:34 -0600

    Created initial state

--- src/Board.js
+++ src/Board.js
@@ -2,44 +2,31 @@ import React from "react";
 import Cell from "./Cell";
 import "./Board.css";
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends React.Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25,
+  };
+
   constructor(props) {
     super(props);
 
-    // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard(),
+    };
   }
-
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      board.push(row);
+    }
     return board;
   }
 
@@ -71,15 +58,7 @@ class Board extends React.Component {
   render() {
     // if the game is won, just show a winning msg & render nothing else
     // TODO
-    // make table board
-    // TODO
-    return(
-      <table className="Board">
-        <tbody>
-          <tr><Cell /></tr>
-        </tbody>
-      </table>
-    )
+    return <h1>Board</h1>;
   }
 }
 
