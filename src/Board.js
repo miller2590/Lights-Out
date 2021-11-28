1efadb6ea5e6780703e642528a1fdc261a87db14
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-11-28 00:16:33 -0600

    game logic complete

--- src/Board.js
+++ src/Board.js
@@ -46,35 +46,46 @@ class Board extends React.Component {
     }
 
     // TODO: flip this cell and the cells around it
+    flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
-    // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    // this.setState({ board, hasWon });
+    let hasWon = board.every((row) => row.every((cell) => !cell));
+
+    this.setState({ board: board, hasWon: hasWon });
   }
 
   /** Render game board or winning message. */
 
   render() {
-    // if the game is won, just show a winning msg & render nothing else
-    // TODO
-    let tblBoard = []
+    if (this.state.hasWon) {
+      return <h1>You Win!!</h1>;
+    }
+    let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
-        let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>)
+        let coord = `${y}-${x}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
-      tblBoard.push(<tr key={y}>{row}</tr>)
+      tblBoard.push(<tr key={y}>{row}</tr>);
     }
 
     return (
       <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
+        <tbody>{tblBoard}</tbody>
       </table>
-    )
+    );
   }
 }
 
